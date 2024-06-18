ed7ab3b5bff77e5b2a6cee7080faa3351d99992d
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-06-18 19:00:17 +0200

    [core) setup vue-query

--- apps/webfolio/src/App.vue
+++ apps/webfolio/src/App.vue
@@ -3,7 +3,10 @@
 </template>
 
 <script setup lang="ts">
+import { useQueryClient } from '@tanstack/vue-query';
 defineOptions({
   name: 'App'
 });
+
+const queryClient = useQueryClient();
 </script>
