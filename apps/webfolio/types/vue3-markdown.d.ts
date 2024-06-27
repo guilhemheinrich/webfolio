02915cd2ca7743ed0515efd703055507e6fa288c
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-06-27 15:59:15 +0200

    [core] adding markdown support

---
+++ apps/webfolio/types/vue3-markdown.d.ts
@@ -0,0 +1,6 @@
+declare module 'vue3-markdown' {
+  // Déclarez les types ici si vous en connaissez.
+  // Si vous ne connaissez pas les types exacts, vous pouvez utiliser `any` comme type générique.
+  export const VueMarkdown: unknown;
+  export const VMarkdownView: unknown;
+}
