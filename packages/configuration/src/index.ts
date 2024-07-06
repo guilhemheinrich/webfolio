7dec5590989138bd05223be61e61033e89764eb0
guilhem.heinrich <guilhem.heinrich@gmail.com>
2024-07-06 13:55:23 +0200

    [Feat] Added service key to configuration

--- packages/configuration/src/index.ts
+++ packages/configuration/src/index.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
 const ConfigSchema = z.object({
-    SUPABASE_URL: z.string(),
-    SUPABASE_ANON_KEY: z.string(),
+  SUPABASE_URL: z.string(),
+  SUPABASE_ANON_KEY: z.string(),
+  SUPABASE_SERVICE_KEY: z.string(),
 });
 
 export type Config = z.infer<typeof ConfigSchema>;
 const rawConfig: Partial<Config> = {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
 };
 export const Config = ConfigSchema.parse(rawConfig);
