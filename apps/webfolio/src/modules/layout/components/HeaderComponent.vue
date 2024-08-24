998a04fb3defddb5b75fc0d9737415ba3a167c80
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-08-24 17:41:32 +0200

    [Feat] Added social media handling

--- apps/webfolio/src/modules/layout/components/HeaderComponent.vue
+++ apps/webfolio/src/modules/layout/components/HeaderComponent.vue
@@ -1,34 +1,121 @@
 <template>
+  <q-toolbar v-if="Config.DEV">
+    <q-btn
+      class="tw-w-full"
+      color="brand"
+      v-if="!editionStore.editable"
+      @click="editionStore.editable = true"
+    >
+      Toggle Admin Mode
+    </q-btn>
+    <q-btn
+      class="tw-w-full"
+      color="brand"
+      v-if="editionStore.editable"
+      @click="editionStore.editable = false"
+    >
+      Toggle User View
+    </q-btn>
+  </q-toolbar>
+
   <q-toolbar>
     <q-toolbar-title>
-      <div class="row no-wrap gt-sm items-center justify-between tw-h-[15vh]">
-        <div class="text-brand tw-w-2/12">
-          {{ add2(3) }}
-
-          My Webfolio
-        </div>
+      <div class="row no-wrap gt-sm items-center tw-h-[15vh] tw-gap-8">
         <q-img
           src="~assets/logo_lite.png"
-          fit="scale-down"
-          class="tw-w-8/12"
+          class="img-circle tw-w-8/12"
+          alt="Profil's picture"
         ></q-img>
-        <div v-if="Config.DEV" class="tw-w-2/12">
-          <q-btn
-            class="tw-w-full"
-            color="brand"
-            v-if="!editionStore.editable"
-            @click="editionStore.editable = true"
-          >
-            Toggle Admin Mode
-          </q-btn>
-          <q-btn
-            class="tw-w-full"
-            color="brand"
-            v-if="editionStore.editable"
-            @click="editionStore.editable = false"
-          >
-            Toggle User View
-          </q-btn>
+
+        <div style="height: 100%">
+          <h2 class="profil_quote">
+            Profil quote : Tell something quick about you
+          </h2>
+        </div>
+
+        <div class="column justify-between tw-h-full">
+          Contacts
+
+          <ul class="row tw-gap-2">
+            <!-- Bouton GitHub -->
+            <li v-if="Config.GITHUB_PROFIL">
+              <a
+                :href="Config.GITHUB_PROFIL"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="GitHub"
+              >
+                <q-icon name="fa-brands fa-github" size="lg" />
+              </a>
+            </li>
+
+            <!-- Bouton LinkedIn -->
+            <li v-if="Config.LINKDIN_PROFIL">
+              <a
+                :href="Config.LINKDIN_PROFIL"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn"
+              >
+                <q-icon name="fa-brands fa-linkedin" size="lg" />
+              </a>
+            </li>
+
+            <!-- Bouton Facebook -->
+            <li v-if="Config.FACEBOOK_PROFIL">
+              <a
+                :href="Config.FACEBOOK_PROFIL"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Facebook"
+              >
+                <q-icon name="fa-brands fa-facebook" size="lg" />
+              </a>
+            </li>
+
+            <!-- Bouton Twitter -->
+            <li v-if="Config.TWITTER_PROFIL">
+              <a
+                :href="Config.TWITTER_PROFIL"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Twitter"
+              >
+                <q-icon name="fa-brands fa-x-twitter" size="lg" />
+              </a>
+            </li>
+
+            <!-- Bouton Email -->
+            <li v-if="Config.CONTACT_MAIL">
+              <a :href="`mailto:${Config.CONTACT_MAIL}`" aria-label="Email">
+                <q-icon name="fa-solid fa-envelope" size="lg" />
+              </a>
+            </li>
+
+            <!-- Discord server // not yet -->
+            <!-- <li v-if="Config.DISCORD_SERVER">
+              <a
+                :href="Config.DISCORD_SERVER"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Discord"
+              >
+                <q-icon name="fa-brands fa-discord" size="lg" />
+              </a>
+            </li> -->
+
+            <!-- Bouton Instagram -->
+            <li v-if="Config.INSTAGRAM_PROFIL">
+              <a
+                :href="Config.INSTAGRAM_PROFIL"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram"
+              >
+                <q-icon name="fa-brands fa-instagram" size="lg" />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </q-toolbar-title>
@@ -43,4 +130,6 @@ const editionStore = useEditionStore();
 add2(3);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$square-profil-dimension: 130px;
+</style>
--- packages/configuration/src/index.ts
+++ packages/configuration/src/index.ts
@@ -1,15 +1,29 @@
 import { z } from "zod";
 
 const ConfigSchema = z.object({
-  SUPABASE_URL: z.string(),
-  SUPABASE_ANON_KEY: z.string(),
-  SUPABASE_SERVICE_KEY: z.string(),
+    SUPABASE_URL: z.string(),
+    SUPABASE_ANON_KEY: z.string(),
+    SUPABASE_SERVICE_KEY: z.string(),
+    CONTACT_MAIL: z.string().optional(),
+    GITHUB_PROFIL: z.string().optional(),
+    LINKDIN_PROFIL: z.string().optional(),
+    FACEBOOK_PROFIL: z.string().optional(),
+    TWITTER_PROFIL: z.string().optional(),
+    DISCORD_SERVER: z.string().optional(),
+    INSTAGRAM_PROFIL: z.string().optional(),
 });
 
 export type Config = z.infer<typeof ConfigSchema>;
 const rawConfig: Partial<Config> = {
-  SUPABASE_URL: process.env.SUPABASE_URL,
-  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-  SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+    CONTACT_MAIL: process.env.CONTACT_MAIL,
+    GITHUB_PROFIL: process.env.GITHUB_PROFIL,
+    LINKDIN_PROFIL: process.env.LINKDIN_PROFIL,
+    FACEBOOK_PROFIL: process.env.FACEBOOK_PROFIL,
+    TWITTER_PROFIL: process.env.TWITTER_PROFIL,
+    INSTAGRAM_PROFIL: process.env.INSTAGRAM_PROFIL,
+    DISCORD_SERVER: process.env.DISCORD_SERVER,
 };
 export const Config = ConfigSchema.parse(rawConfig);
