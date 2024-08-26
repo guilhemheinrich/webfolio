f4b260c33fa8cb5c165c4fb886706aae9517937d
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-08-26 13:24:59 +0200

    [Feat] Small hovering effect and redirection to home when clicking logo

--- apps/webfolio/src/modules/layout/components/HeaderComponent.vue
+++ apps/webfolio/src/modules/layout/components/HeaderComponent.vue
@@ -6,14 +6,16 @@
           class="row no-wrap items-center justify-between tw-h-[15dvh] tw-gap-8"
         >
           <figure style="height: 100%" class="col-grow q-mx-xl row">
-            <q-img
-              src="~assets/logo_lite.png"
-              class="img-circle"
-              alt="Profil's picture"
-            ></q-img>
+            <router-link to="/" class="no-decoration">
+              <q-img
+                src="~assets/logo_lite.png"
+                class="img-circle"
+                alt="Profil's picture"
+              ></q-img>
+            </router-link>
 
-            <figcaption class="img-caption">
-              Profil quote : Tell something quick about you
+            <figcaption class="img-caption text-secondary text-italic">
+              &ldquo; Profil quote : Tell something quick about you &rdquo;
             </figcaption>
           </figure>
 
@@ -45,6 +47,10 @@ $square-profil-dimension: 110px;
   border-radius: 50%; /* Cela transforme l'image en cercle */
   background-color: white;
   transform: scale(1.4);
+}
+
+.img-circle:hover {
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.6); /* Ombre plus intense au hover */
 }
 
 .img-caption {
