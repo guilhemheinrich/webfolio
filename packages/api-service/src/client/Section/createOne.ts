0b8dc26d3000fe64a6f253cbf8514ded9b9ddd24
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-09-09 13:59:02 +0200

    [Fix] upsert instead of insert on section creation, to avoid error

--- packages/api-service/src/client/Section/createOne.ts
+++ packages/api-service/src/client/Section/createOne.ts
@@ -3,10 +3,12 @@ import { SupabaseTypedClient } from "../../supabase";
 
 export const createOneSection = (supabase: SupabaseTypedClient) =>
     new ApiCall(async (parameters: { section_slug: string }) => {
+        // We use upsert here to prevent error while trying te create an already existing section
+        // By providing the primary key, there is no error nor creation in the relavant case
         const { data: webfolio_section_data, error: webfolio_section_error } =
             await supabase
                 .from("webfolio_section")
-                .insert({
+                .upsert({
                     slug: parameters.section_slug,
                 })
                 .select();
