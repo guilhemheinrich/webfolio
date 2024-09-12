f1ec1425ed4fc3367de9a44ae3ab52412434fd2a
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-09-12 16:40:02 +0200

    [Fix] filtering experience by decreasing start date

--- packages/api-service/src/client/Experience/fetch.ts
+++ packages/api-service/src/client/Experience/fetch.ts
@@ -6,11 +6,16 @@ export const FetchExperiences = (supabase: SupabaseTypedClient) =>
         const {
             data: webfolio_experience_data,
             error: webfolio_experience_error,
-        } = await supabase.from("webfolio_experience").select(`
+        } = await supabase
+            .from("webfolio_experience")
+            .select(
+                `
                 *
                 , webfolio_experience_short_description(*)
                 , webfolio_experience_description(*)
-                , webfolio_experience_title(*)`);
+                , webfolio_experience_title(*)`,
+            )
+            .order("start_date", { ascending: false });
         if (webfolio_experience_error) throw webfolio_experience_error;
         return webfolio_experience_data;
     });
