8375c1c49db461daba33d5699b38c0155149cadb
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-09-14 15:53:02 +0200

    [fix] no process.env leak

--- apps/webfolio/src/modules/configuration/index.ts
+++ apps/webfolio/src/modules/configuration/index.ts
@@ -17,9 +17,10 @@ export const Config = {
   ...ConfigWebfolio,
 };
 
-console.log('ConfigWebfolio.ADMIN', ConfigWebfolio.ADMIN);
-console.log('process.env.ADMIN', process.env.ADMIN);
-console.log(
-  'Boolean(JSON.parse(process.env.ADMIN ?? "")',
-  Boolean(JSON.parse(process.env.ADMIN ?? '')),
-);
+// console.log('ConfigWebfolio.ADMIN', ConfigWebfolio.ADMIN);
+// console.log('process.env.ADMIN', process.env.ADMIN);
+// console.log(
+//   'Boolean(JSON.parse(process.env.ADMIN ?? "")',
+//   Boolean(JSON.parse(process.env.ADMIN ?? '')),
+// );
+// console.log('process.env', process.env);
