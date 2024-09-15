4099bd7825bb3ebc8e560f586adc720dc039c7f7
guilhemheinrich <guilhem.heinrich@gmail.com>
2024-09-15 14:55:21 +0200

    messy stuff

--- apps/webfolio/src/modules/UI/components/form/PictureInput.vue
+++ apps/webfolio/src/modules/UI/components/form/PictureInput.vue
@@ -10,10 +10,14 @@
         :hide-upload-btn="true"
       />
 
-      <!-- Afficher l'aperçu de l'image sélectionnée -->
-      <!-- <div v-if="imageSrc" class="q-mt-md">
-        <q-img :src="imageSrc" :alt="imageName" style="max-width: 300px" />
-      </div> -->
+      <Cropper
+        ref="cropper"
+        class="cropper"
+        :src="img"
+        :stencil-props="{
+          aspectRatio: 500 / 300,
+        }"
+      ></Cropper>
     </q-card-section>
     <q-card-actions align="around">
       <q-btn color="secondary" @click="emit('cancel')"> Cancel </q-btn>
@@ -22,7 +26,10 @@
   </q-card>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { DefineComponent, Ref, ref } from 'vue';
+import { QCarousel } from 'quasar';
+import { Cropper } from 'vue-advanced-cropper';
+import 'vue-advanced-cropper/dist/style.css';
 
 const uploader = ref();
 const emit = defineEmits<{
@@ -30,9 +37,25 @@ const emit = defineEmits<{
   (e: 'cancel'): void;
 }>();
 
-// Déclaration des variables réactives
+type ExtractMethods<T> =
+  T extends DefineComponent<unknown, infer Methods> ? Methods : never;
+
+type CropperMethods = ExtractMethods<typeof Cropper>;
+const cropper: Ref<CropperMethods | null> = ref(null);
+const croppedImage: Ref<string | null> = ref(null);
+
+// Fonction pour récupérer l'image à l'intérieur du stencil
+const cropImage = () => {
+  if (cropper.value) {
+    const canvas = cropper.value.getResult().canvas;
+    // croppedImage.value = canvas.toDataURL('image/jpeg');
+  }
+};
 
 const pictureValidated = () => {
+  cropImage();
+  console.log(croppedImage.value);
+  // Here use
   if (uploader.value && uploader.value.files.length > 0) {
     const files = uploader.value ? uploader.value.files : []; // Récupérer les fichiers
 
@@ -41,4 +64,12 @@ const pictureValidated = () => {
     }
   }
 };
+
+const img = ref(
+  'https://images.pexels.com/photos/226746/pexels-photo-226746.jpeg',
+);
+
+// const change = (parameters: { coordinates: any; canvas: any }) => {
+//   console.log(parameters.coordinates, parameters.canvas);
+// };
 </script>
